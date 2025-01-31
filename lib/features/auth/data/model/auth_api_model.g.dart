7a4534df3a8e8347cb26818f04714ce563a1af73
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-01-31 14:53:06 +0545

    auth api model repo and use case

---
+++ lib/features/auth/data/model/auth_api_model.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_api_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthApiModel _$AuthApiModelFromJson(Map<String, dynamic> json) => AuthApiModel(
+      id: json['_id'] as String?,
+      freelancerName: json['freelancerName'] as String,
+      portfolio: json['portfolio'] as String,
+      profileImage: json['profileImage'] as String?,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      skills: (json['skills'] as List<dynamic>)
+          .map((e) => SkillApiModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      availability: json['availability'] as String,
+      experienceYears: (json['experienceYears'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$AuthApiModelToJson(AuthApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'freelancerName': instance.freelancerName,
+      'profileImage': instance.profileImage,
+      'skills': instance.skills,
+      'email': instance.email,
+      'password': instance.password,
+      'portfolio': instance.portfolio,
+      'availability': instance.availability,
+      'experienceYears': instance.experienceYears,
+    };
--- lib/features/auth/domain/use_case/register_usecase.dart
+++ lib/features/auth/domain/use_case/register_usecase.dart
@@ -1,48 +1,74 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-
-import '../../../../app/usecase/usecase.dart';
-import '../../../../core/error/failure.dart';
-import '../entity/auth_entity.dart';
-import '../repository/auth_repository.dart';
+import 'package:projectsyeti/app/usecase/usecase.dart';
+import 'package:projectsyeti/core/error/failure.dart';
+import 'package:projectsyeti/features/auth/domain/entity/auth_entity.dart';
+import 'package:projectsyeti/features/auth/domain/repository/auth_repository.dart';
+import 'package:projectsyeti/features/skill/domain/entity/skill_entity.dart';
 
 class RegisterUserParams extends Equatable {
+  final String freelancerName;
+  final String? profileImage;
+  final List<SkillEntity> skills;
   final String email;
   final String password;
-  final String fname;
-  final String phone;
+  final String portfolio;
+  final String availability;
+  final int experienceYears;
 
   const RegisterUserParams({
+    required this.freelancerName,
+    required this.portfolio,
+    this.profileImage,
     required this.email,
     required this.password,
-    required this.fname,
-    required this.phone,
+    required this.skills,
+    required this.availability,
+    required this.experienceYears,
   });
 
-  const RegisterUserParams.initial()
-      : email = '',
-        password = '',
-        fname = '',
-        phone = '';
+  //initial constructor
+  const RegisterUserParams.initial({
+    required this.freelancerName,
+    required this.portfolio,
+    this.profileImage,
+    required this.email,
+    required this.password,
+    required this.skills,
+    required this.availability,
+    required this.experienceYears,
+  });
 
   @override
-  List<Object?> get props => [email, password, fname, phone];
+  // TODO: implement props
+  List<Object?> get props => [
+        freelancerName,
+        portfolio,
+        profileImage,
+        email,
+        password,
+        skills,
+        availability,
+        experienceYears
+      ];
 }
 
 class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
   final IAuthRepository repository;
-
   RegisterUseCase(this.repository);
 
   @override
   Future<Either<Failure, void>> call(RegisterUserParams params) {
     final authEntity = AuthEntity(
-      id: null,
+      freelancerName: params.freelancerName,
+      portfolio: params.portfolio,
       email: params.email,
+      skills: params.skills,
+      availability: params.availability,
+      experienceYears: params.experienceYears,
       password: params.password,
-      fname: params.fname,
-      phone: params.phone,
+      profileImage: params.profileImage,
     );
-    return repository.registerStudent(authEntity);
+    return repository.registerUser(authEntity);
   }
 }
