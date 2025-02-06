6f71e29d5026a06fe7dc746cabf617c1213eeb9c
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-02-06 14:12:16 +0545

    testing - get all skills usecase

--- lib/features/skill/domain/repository/skill_repository.dart
+++ lib/features/skill/domain/repository/skill_repository.dart
@@ -4,6 +4,4 @@ import 'package:projectsyeti/features/skill/domain/entity/skill_entity.dart';
 
 abstract interface class ISkillRepository {
   Future<Either<Failure, List<SkillEntity>>> getSkills();
-  Future<Either<Failure, void>> createSkills(SkillEntity skill);
-  Future<Either<Failure, void>> deleteSkills(String id);
 }
